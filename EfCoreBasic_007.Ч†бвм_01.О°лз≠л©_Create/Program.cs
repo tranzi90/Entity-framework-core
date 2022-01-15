6c319a8b53f2d3d503ab48634b957ba880048713
denis_levchenko <tranzi90@gmail.com>
2022-01-15 21:13:39 +0300

    CUD operations

---
+++ EfCoreBasic_007.Ч†бвм_01.О°лз≠л©_Create/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EfCoreBasic_007.Часть_01.Обычный_Create
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Создание_пустой_базы_данных();
+            Добавление_курсов();
+            Чтение_курсов();
+        }
+
+        public static void Создание_пустой_базы_данных()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            dbContext.Database.EnsureDeleted();
+
+            dbContext.Database.EnsureCreated();
+        }
+
+        public static void Добавление_курсов()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            var csharpCourse = new Course
+            {
+                Name = "C# Advanced",
+                LessonsQuantity = 7
+            };
+
+            var efCoreCourse = new Course
+            {
+                Name = "Entity Framework Core Basic",
+                LessonsQuantity = 10,
+            };
+
+            // добавляем курсы в контекст EF Core
+            // здесь EF Core ещё не выполняет никаких запросов в БД
+            dbContext.Add(csharpCourse);
+            dbContext.Add(efCoreCourse);
+
+            // уже здесь EF Core выполняет команду, которая сохраняет курсы в БД
+            dbContext.SaveChanges();
+        }
+
+        public static void Чтение_курсов()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            var courses = dbContext.Courses.ToList();
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(
+                    $"Название курса: {course.Name}. Количество уроков: {course.LessonsQuantity}.");
+            }
+        }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=EfCoreBasicDb;Trusted_Connection=True;")
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging()
+                .LogTo(
+                    Console.WriteLine,
+                    new[] { DbLoggerCategory.Database.Command.Name },
+                    LogLevel.Information);
+        }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int LessonsQuantity { get; set; }
+    }
+}
---
+++ EfCoreBasic_007.Ч†бвм_09.Delete_бгй≠Ѓбв®_Ґ_™Ѓ≠в•™бв•/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EfCoreBasic_007.Часть_09.Delete_сущности_в_контексте
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Создание_пустой_базы_данных();
+            Добавление_данных();
+            Чтение_данных();
+            Удаление_сущности_в_контексте();
+            Чтение_данных();
+        }
+
+        public static void Создание_пустой_базы_данных()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            dbContext.Database.EnsureDeleted();
+
+            dbContext.Database.EnsureCreated();
+        }
+
+        public static void Добавление_данных()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            var csharpCourse = new Course
+            {
+                Name = "C# Advanced",
+                LessonsQuantity = 7
+            };
+
+            var efCoreCourse = new Course
+            {
+                Name = "Entity Framework Core Basic",
+                LessonsQuantity = 10,
+            };
+
+            var unitTestsCourse = new Course
+            {
+                Name = "Юнит-тестирование",
+                LessonsQuantity = 3,
+            };
+
+            var johnSmith = new Author
+            {
+                FirstName = "John",
+                LastName = "Smith"
+            };
+
+            var arthurMorgan = new Author
+            {
+                FirstName = "Arthur",
+                LastName = "Morgan"
+            };
+
+            csharpCourse.Author = johnSmith;
+            efCoreCourse.Author = johnSmith;
+            unitTestsCourse.Author = arthurMorgan;
+
+            dbContext.Add(csharpCourse);
+            dbContext.Add(efCoreCourse);
+            dbContext.Add(unitTestsCourse);
+            dbContext.Add(arthurMorgan);
+
+            dbContext.SaveChanges();
+        }
+
+        public static void Чтение_данных()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            var courses = dbContext
+                .Courses
+                .Include(x => x.Author)
+                .ToList();
+
+            Console.WriteLine(
+                new string(
+                    '-',
+                    80));
+            Console.WriteLine("Информация о курсах.");
+            foreach (var course in courses)
+            {
+                Console.WriteLine(
+                    $"Имя курса: {course.Name}. " +
+                    $"Идентификатор курса: {course.Id}. " +
+                    $"Количество уроков: {course.LessonsQuantity}. " +
+                    $"Автор: {course.Author.FirstName + " " + course.Author.LastName}.");
+            }
+
+            Console.WriteLine(
+                new string(
+                    '-',
+                    80));
+
+            var authors = dbContext
+                .Authors
+                .ToList();
+
+            Console.WriteLine(
+                new string(
+                    '-',
+                    80));
+            Console.WriteLine("Информация об авторах.");
+            foreach (var author in authors)
+            {
+                Console.WriteLine($"Автор: {author.FirstName + " " + author.LastName}");
+            }
+
+            Console.WriteLine(
+                new string(
+                    '-',
+                    80));
+        }
+
+        public static void Удаление_сущности_в_контексте()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            var firstCourse = dbContext
+                .Courses
+                .First();
+
+            // Просто удаляем курс
+            dbContext.Remove(firstCourse);
+
+            dbContext.SaveChanges();
+        }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=EfCoreBasicDb;Trusted_Connection=True;")
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging()
+                .LogTo(
+                    Console.WriteLine,
+                    new[] { DbLoggerCategory.Database.Command.Name },
+                    LogLevel.Information);
+        }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int LessonsQuantity { get; set; }
+
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
